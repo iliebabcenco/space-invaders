16d422f131746a57e11a6cf50f123275dfce3b44
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-04-24 12:49:11 +0300

    Add ufos, bombs, rockets, ship, width, height for Space class, getters, constructor

--- src/Space.java
+++ src/Space.java
@@ -1,5 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Space {
+    private int width;
+    private int height;
+    private SpaceShip ship;
+    private List<Ufo> ufos = new ArrayList<>();
+    private List<Rocket> rockets = new ArrayList<>();
+    private List<Bomb> bombs = new ArrayList<>();
+
     public static void main(String[] args) {
 
+    }
+
+    public Space(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public SpaceShip getShip() {
+        return ship;
+    }
+
+    public List<Ufo> getUfos() {
+        return ufos;
+    }
+
+    public List<Rocket> getRockets() {
+        return rockets;
+    }
+
+    public List<Bomb> getBombs() {
+        return bombs;
+    }
+
+    public void setShip(SpaceShip ship) {
+        this.ship = ship;
     }
 }
