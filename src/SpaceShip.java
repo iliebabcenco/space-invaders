c95960ca8d44a017a1329d9b92134a2357a5bc35
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-04-24 18:49:55 +0300

    Add fire draw, move methods for SpaceShip

--- src/SpaceShip.java
+++ src/SpaceShip.java
@@ -12,4 +12,18 @@ public class SpaceShip extends BaseObject{
     public void moveRight() {
         dx = 1;
     }
+
+    public void move() {
+        x += dx;
+        this.checkBorders(0, Space.game.getWidth(), 0, Space.game.getHeight());
+    }
+
+    public void draw(Canvas canvas) {}
+
+    public void fire() {
+        Rocket rocket1 = new Rocket(x + 2, y);
+        Rocket rocket2 = new Rocket(x - 2, y);
+        Space.game.getRockets().add(rocket1);
+        Space.game.getRockets().add(rocket2);
+    }
 }
