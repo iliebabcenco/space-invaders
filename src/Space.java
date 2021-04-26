cfddc16c108c790ce10d1bc84cb44dc559fb47a4
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-04-26 08:19:20 +0300

    Add methods to Space class

--- src/Space.java
+++ src/Space.java
@@ -1,3 +1,4 @@
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +18,8 @@ public class Space {
         this.width = width;
         this.height = height;
     }
+
+
 
     public int getWidth() {
         return width;
@@ -47,14 +50,97 @@ public class Space {
     }
 
     public void run() {
+        //Создаем холст для отрисовки.
+        Canvas canvas = new Canvas(width, height);
 
+        //Создаем объект "наблюдатель за клавиатурой" и стартуем его.
+        KeyboardObserver keyboardObserver = new KeyboardObserver();
+        keyboardObserver.start();
+
+        //Игра работает, пока корабль жив
+        while (ship.isAlive()) {
+            //"наблюдатель" содержит события о нажатии клавиш?
+            if (keyboardObserver.hasKeyEvents()) {
+                KeyEvent event = keyboardObserver.getEventFromTop();
+                //Если "стрелка влево" - сдвинуть фигурку влево
+                System.out.print(event.getKeyCode());
+                if (event.getKeyCode() == KeyEvent.VK_LEFT)
+                    ship.moveLeft();
+                    //Если "стрелка вправо" - сдвинуть фигурку вправо
+                else if (event.getKeyCode() == KeyEvent.VK_RIGHT)
+                    ship.moveRight();
+                    //Если "пробел" - стреляем
+                else if (event.getKeyCode() == KeyEvent.VK_SPACE)
+                    ship.fire();
+            }
+
+            //двигаем все объекты игры
+            moveAllItems();
+
+            //проверяем столкновения
+            checkBombs();
+            checkRockets();
+            //удаляем умершие объекты из списков
+            removeDead();
+
+            //Создаем НЛО (1 раз в 10 ходов)
+            createUfo();
+
+            //Отрисовываем все объекты на холст, а холст выводим на экран
+            canvas.clear();
+            draw(canvas);
+            canvas.print();
+
+            //Пауза 300 миллисекунд
+            Space.sleep(300);
+        }
+
+        //Выводим сообщение "Game Over"
+        System.out.println("Game Over!");
     }
 
-    public void draw() {
-
+    public void moveAllItems() {
+        //нужно получить список всех игрвых объектов и у каждого вызвать метод move().
+        List<BaseObject> list = this.getAllItems();
+        for (BaseObject baseObject : list) {
+            baseObject.move();
+        }
     }
 
-    public void sleep(int ms) {
+    public List<BaseObject> getAllItems() {
+        //нужно создать новый список и положить в него все игровые объекты.
+        List<BaseObject> baseObjects = new ArrayList<>();
+        baseObjects.add(this.ship);
+        baseObjects.addAll(this.bombs);
+        baseObjects.addAll(this.rockets);
+        baseObjects.addAll(this.ufos);
+        return baseObjects;
+    }
 
+    public void createUfo() {
+        //тут нужно создать новый НЛО.
+    }
+
+    public void checkBombs() {
+        //тут нужно проверить все возможные столкновения для каждой бомбы.
+    }
+
+    public void checkRockets() {
+        //тут нужно проверить все возможные столкновения для каждой ракеты.
+    }
+
+    public void removeDead() {
+        //тут нужно удалить все умершие объекты из списков (кроме космического корабля)
+    }
+
+    public void draw(Canvas canvas) {
+        //тут нужно отрисовать все объекты игры
+    }
+
+    public static void sleep(int delay) {
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException ignored) {
+        }
     }
 }
